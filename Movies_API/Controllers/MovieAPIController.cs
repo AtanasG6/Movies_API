471e21218ac90f30d0692dd66cb8fefb1a45c3b9
atanas.ig <gyulchev666@gmail.com>
2024-04-15 20:39:27 +0300

    Controller, Model, DTO

---
+++ Movies_API/Controllers/MovieAPIController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies_API.Models;
+using Movies_API.Models.Dto;
+
+namespace Movies_API.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/MovieAPI")]
+    [ApiController]
+    public class MovieAPIController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<MovieDTO> GetMovies()
+        {
+            return new List<MovieDTO>() { 
+                new MovieDTO() { Id = 1, Name = "Lord of the Rings" },
+                new MovieDTO() { Id = 2, Name = "Jaws" },
+                
+            };
+        }
+    }
+}
---
+++ Movies_API/Models/Dto/MovieDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Movies_API.Models.Dto
+{
+    public class MovieDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
